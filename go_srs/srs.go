25a2a905c617e3c6c6406fee87ec3e43be9466e3
winlin <winlin@vip.126.com>
2014-02-13 22:00:55 +0800

    add ReadBasicHeader from c++ srs

--- go_srs/srs.go
+++ go_srs/srs.go
@@ -52,10 +52,10 @@ func main() {
 			return;
 		}
 
-		simple_handshake := func(conn *net.TCPConn) (err error) {
+		SimpleHandshake := func(conn *net.TCPConn) (err error) {
 			c0c1 := make([]byte, 1537)
 			// TODO: FIXME: read in block mode.
-			nsize,err := conn.Read(c0c1)
+			nsize, err := conn.Read(c0c1)
 			if err != nil {
 				return
 			}
@@ -64,14 +64,14 @@ func main() {
 			s0s1s2 := make([]byte, 3073)
 			copy(s0s1s2[0:1537], c0c1)
 			// TODO: FIXME: write in block mode.
-			nsize,err = conn.Write(s0s1s2)
+			nsize, err = conn.Write(s0s1s2)
 			if err != nil {
 				return
 			}
 			fmt.Println("write s0s1s2, size=", nsize)
 
 			c2 := make([]byte, 1536)
-			nsize,err = conn.Read(c2)
+			nsize, err = conn.Read(c2)
 			if err != nil {
 				return
 			}
@@ -80,13 +80,73 @@ func main() {
 			return
 		}
 
+		ReadBasicHeader := func(conn *net.TCPConn) (format byte, cid int, bh_size int, err error) {
+			buf := make([]byte, 1)
+			_, err = conn.Read(buf)
+			if err != nil {
+				return
+			}
+
+			format = buf[0]
+			cid = int(format) & 0x3f
+			format = (format >> 6) & 0x03
+			bh_size = 1
+
+			if cid > 1 {
+				return
+			}
+
+			if cid == 0 {
+				_, err = conn.Read(buf)
+				if err != nil {
+					return
+				}
+				cid = 64
+				cid += int(buf[0])
+				bh_size = 2
+				return
+			}
+
+			// cid is 1
+			buf = make([]byte, 2)
+			_, err = conn.Read(buf)
+			if err != nil {
+				return
+			}
+			cid = 64
+			cid += int(buf[0])
+			cid += int(buf[1]) * 256
+			bh_size = 3
+
+			return
+		}
+
+		do_serve :=func(conn *net.TCPConn) (err error) {
+			err = SimpleHandshake(conn)
+			if err != nil {
+				return
+			}
+
+			format, cid, bhsize, err := ReadBasicHeader(conn)
+			if err != nil {
+				return
+			}
+			fmt.Printf("fmt=%v, cid=%v, bhsize=%v\n", format, cid, bhsize)
+
+			return
+		}
+
 		serve := func(conn *net.TCPConn) {
+			defer conn.Close()
+
 			fmt.Println("get client:", conn.RemoteAddr())
-			err := simple_handshake(conn)
+			err := do_serve(conn)
 			if err != nil {
 				fmt.Println("error:", err)
 				return
 			}
+
+			fmt.Println("serve client completed")
 		}
 		go serve(conn)
 	}
--- research/demo/tour/go_types.go
+++ research/demo/tour/go_types.go
@@ -5,10 +5,24 @@ import (
 	"reflect"
 )
 
+type Winlin struct {
+	name string
+	age int
+	email string
+}
+
 func main() {
 	var a interface {} = 10
 	fmt.Println(a)
 	fmt.Println(reflect.TypeOf(a))
 	fmt.Println(a.(int) + 10)
 	fmt.Println(reflect.ValueOf(a).Int() + 10)
+
+	pw := new(Winlin)
+	fmt.Printf("winlin, name=%v, age=%v, email=%v\n", pw.name, pw.age, pw.email)
+
+	pw = &Winlin{"winlin", 28, "winterserver@126.com"}
+	fmt.Printf("winlin, name=%v, age=%v, email=%v\n", pw.name, pw.age, pw.email)
+	pin := *pw
+	fmt.Printf("winlin, name=%v, age=%v, email=%v\n", pin.name, pin.age, pin.email)
 }
